4c5ca73d2dac1626c46b2fad240bfa1b3cf6def5
astalberto <albertoherrera205@hotmail.com>
2026-05-21 12:31:17 -0500

    Load clubs and players from txt files

--- ejemplo02/ingreso_datos.py
+++ ejemplo02/ingreso_datos.py
@@ -14,13 +14,35 @@ session = Session()
 
 with open("data/datos_clubs.txt", "r", encoding="utf-8") as file:
     for line in file:
+
         nombre, deporte, fundacion = line.strip().split(";")
 
-        print("Name:", nombre)
+        club = Club(
+            nombre=nombre,
+            deporte=deporte,
+            fundacion=int(fundacion)
+        )
 
-        
+        session.add(club)
+
+session.commit()
 with open("data/datos_jugadores.txt", "r", encoding="utf-8") as file:
     for line in file:
-        club, posicion, dorsal, nombre  = line.strip().split(";")
 
-        print("Name:", nombre)
+        club_nombre, posicion, dorsal, nombre = line.strip().split(";")
+
+        # search club in database
+        club_busqueda = session.query(Club)\
+                               .filter_by(nombre=club_nombre)\
+                               .one()
+
+        jugador = Jugador(
+            nombre=nombre,
+            dorsal=int(dorsal),
+            posicion=posicion,
+            club=club_busqueda
+        )
+
+        session.add(jugador)
+
+session.commit()
